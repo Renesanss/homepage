e5126736222fc7639159c65a70a35446350c58ae
Renata Pientkowska <pientkowska.renata@gmail.com>
2022-11-02 15:34:09 +0100

    js corrected

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-console.log("Siemka!");
-
-let section__button = document.querySelector(".section__button");
-let section__photo = document.querySelector(".section__photo");
-
-
-section__button.addEventListener("click", () => {
-    section__photo.remove();
-});
+{
+    const welcome = () => {
+        console.log("Hello world!")
+    }
 
 
-let theme__button = document.querySelector(".theme__button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
+    const theme_buttonClick = () => {
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".themeName");
+        body.classList.toggle("dark");
 
-theme__button.addEventListener("click", () => {
-    body.classList.toggle("dark");
+        themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    };
 
-    themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    const init = () => {
+        const theme__button = document.querySelector(".theme__button");
 
-});
+
+        theme__button.addEventListener("click", theme_buttonClick);
+        welcome();
+    }
+
+    init();
+
+    const section__button = document.querySelector(".section__button");
+    const section__photo = document.querySelector(".section__photo");
+
+    section__button.addEventListener("click", () => {
+        section__photo.remove();
+    });
+
+}
 
